418fff41cdb8aaef149ffc1ff71e6bb64d419293
Charles Lowell <cowboyd@frontside.io>
2017-09-21 10:34:57 -0500

    add filterable

--- src/fn.js
+++ src/fn.js
@@ -9,3 +9,7 @@ import './semigroup/array';
 export { Foldable, foldr, foldl } from './foldable'
 import './foldable/array';
 import './foldable/object';
+
+export { Filterable, filter } from './filterable'
+import './filterable/object';
+import './filterable/array';
--- src/foldable.js
+++ src/foldable.js
@@ -11,3 +11,8 @@ export function foldl(fn, initial, foldable) {
   let { foldl } = Foldable(foldable);
   return foldl(fn, initial, foldable);
 }
+
+export function length(foldable) {
+  let { foldr } = Foldable(foldable);
+  return foldr((len) => len + 1, 0, foldable);
+}
