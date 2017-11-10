c8382e18bcd5e8b1bd418a773410ca49a20b2354
Taras Mankovski <tarasm@gmail.com>
2017-11-10 10:06:22 -0500

    Destructure getPrototypeOf from Object

--- src/semigroup/object.js
+++ src/semigroup/object.js
@@ -1,11 +1,11 @@
 import { Semigroup } from '../semigroup';
 import propertiesOf from 'object.getownpropertydescriptors';
 
-const { assign } = Object;
+const { assign, getPrototypeOf } = Object;
 
 Semigroup.instance(Object, {
   append(o1, o2) {
     let properties = assign({}, propertiesOf(o1), propertiesOf(o2));
-    return Object.create(Object.getPrototypeOf(o1), properties);
+    return Object.create(getPrototypeOf(o1), properties);
   }
 });
