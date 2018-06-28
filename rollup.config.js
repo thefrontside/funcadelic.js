5b524b632729b697ffdf86cb66753e6717c46ed5
Taras Mankovski <tarasm@gmail.com>
2018-06-28 12:55:33 -0400

    Use native classes in Node.js (#49)

--- rollup.config.js
+++ rollup.config.js
@@ -2,42 +2,60 @@ const babel = require("rollup-plugin-babel");
 const filesize = require("rollup-plugin-filesize");
 const pkg = require("./package.json");
 
-const globals = {
-  "invariant": "invariant"
-};
+let external = ["invariant"];
 
-let external = Object.keys(globals);
+let filesizePlugin = filesize({
+  render(opt, size, gzip, bundle) {
+    return `Built: ${bundle.file} ( size: ${size}, gzip: ${gzip})`;
+  }
+});
 
-module.exports = {
-  input: "src/funcadelic.js",
-  external,  
-  output: [
-    {
-      name: "funcadelic",
-      file: pkg.browser,
-      globals,
-      format: "umd"
+module.exports = [
+  {
+    input: "src/funcadelic.js",
+    external,
+    output: {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true
     },
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" }
-  ],
-  plugins: [
-    babel({
-      babelrc: false,
-      comments: false,
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            modules: false
-          }
+    plugins: [
+      babel({
+        babelrc: false,
+        comments: false,
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                node: "6"
+              },
+              modules: false
+            }
+          ]
         ]
-      ]
-    }),
-    filesize({
-      render(opt, size, gzip, bundle) {
-        return `Built: ${bundle.file} ( size: ${size}, gzip: ${gzip})`;
-      }
-    })
-  ]
-};
+      }),
+      filesizePlugin
+    ]
+  },
+  {
+    input: "src/funcadelic.js",
+    external,
+    output: { file: pkg.module, format: "es", sourcemap: true },
+    plugins: [
+      babel({
+        babelrc: false,
+        comments: false,
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              modules: false
+            }
+          ]
+        ]
+      }),
+      filesizePlugin
+    ]
+  }
+];
--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,16 @@
 const rollup = require("rollup");
 const config = require("../rollup.config");
+const PrettyError = require("pretty-error");
+const pe = new PrettyError();
+pe.skipNodeFiles();
+pe.skipPackage("rollup");
 
 module.exports = function() {
-  console.log("\n");
+  console.log("\n"); // eslint-disable-line
   // create a bundle
-  return rollup.rollup(config)
-    .then(bundle => {
-      let built = config.output.map(options => bundle.write(options));
-      return Promise.all(built);
-    })
-    .then(() => console.log())
+  return Promise.all(
+    config.map(config =>
+      rollup.rollup(config).then(bundle => bundle.write(config.output))
+    )
+  ).catch(e => console.log(pe.render(e))); // eslint-disable-line
 };
