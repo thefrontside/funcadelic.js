943603e106441853a99b65fda6359fd6890283d0
Taras Mankovski <tarasm@gmail.com>
2018-12-06 18:47:51 -0500

    Upgraded dependencies and added UMD build on "unpkg" package entry (#61)
    * Upgrade all of the dependencies
    
    * Added UMD build
    
    * Added node 10 and removed node 7

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,46 @@
 const babel = require("rollup-plugin-babel");
 const filesize = require("rollup-plugin-filesize");
 const pkg = require("./package.json");
+const resolve = require("rollup-plugin-node-resolve");
+const commonjs = require('rollup-plugin-commonjs');
+const replace = require('rollup-plugin-replace');
 
 let external = ["invariant"];
 
-let filesizePlugin = filesize({
-  render(opt, size, gzip, bundle) {
-    return `Built: ${bundle.file} ( size: ${size}, gzip: ${gzip})`;
-  }
-});
+let filesizePlugin = filesize();
 
 module.exports = [
+  {
+    input: "src/funcadelic.js",
+    output: {
+      name: 'funcadelic',
+      file: pkg.unpkg,
+      format: "umd"
+    },
+    plugins: [
+      replace({
+        "process.env.NODE_ENV": JSON.stringify('production')
+      }),
+      resolve(),
+      commonjs(),
+      babel({
+        babelrc: false,
+        comments: false,
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                chrome: "58"
+              },
+              modules: false
+            }
+          ]
+        ]
+      }),
+      filesizePlugin
+    ]
+  },
   {
     input: "src/funcadelic.js",
     external,
